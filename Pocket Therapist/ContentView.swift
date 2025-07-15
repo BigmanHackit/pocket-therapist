c8d7c33854eba1263d476db8c962679c602f761c
Bigmanhackit <djhackit5@gmail.com>
2025-07-15 12:11:28 +0100

    Initial Commit

---
+++ Pocket Therapist/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pocket Therapist
+//
+//  Created by Firebrand Dev on 15/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pocket Therapist/Pocket_TherapistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_TherapistApp.swift
+//  Pocket Therapist
+//
+//  Created by Firebrand Dev on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_TherapistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
